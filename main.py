ba49431a05d076d44bdd042a1e5a97fa4a339b19
topstar0505 <luckydev.2350@gmail.com>
2022-01-02 01:50:11 +0800

    setting up

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
 
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
 
+with open("data.json") as file:
+    data = json.load(file)
+
+print(data)
