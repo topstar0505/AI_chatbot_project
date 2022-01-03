d4a5b0565db44d9b7747ad05c8c7761e2a8ae7ee
topstar0505 <luckydev.2350@gmail.com>
2022-01-03 17:27:26 +0800

    continue setting up

--- main.py
+++ main.py
@@ -13,14 +13,22 @@ with open("data.json") as file:
 
 words = []
 labels = []
-docs = []
+docs_x = [] #list of diff pattern
+docs_y = [] #entry for docs_x
 
-for data in data_file["data"]:
+for data in data_file["data"]:  #loading json file
     for pattern in data["patterns"]:
         word = nltk.word_tokenize(pattern)
         words.extend(word)
-        docs.append(pattern)
+        docs_x.append(pattern)
+        docs_y.append(data["tag"]) #for classifying
 
     if data["tag"] not in labels:
         labels.append(data["tag"])
 
+words = [stemmer.stem(w.lower()) for w in words] #convert all word to lower case
+words = sorted(list(set(words))) #remove all duplicate and got a sorted list
+
+labels = sorted(labels)
+
+#making a bag for words
