7abd4d4448075734badc8efaf5ee567d1ab7c7eb
topstar0505 <luckydev.2350@gmail.com>
2022-01-04 15:45:26 +0800

    continue debugging

--- main.py
+++ main.py
@@ -12,58 +12,58 @@ import pickle
 with open("intents.json") as file:
     data = json.load(file)
 
-try:
-    with open("data.pickle", "rb") as f:
-        words, labels, train, output = pickle.load(f)
-except:
-    words = []
-    labels = []
-    docs_x = []
-    docs_y = []
+#try:
+#    with open("data.pickle", "rb") as f:
+#        words, labels, train, output = pickle.load(f)
+#except:
+words = []
+labels = []
+docs_x = []
+docs_y = []
 
-    for intent in data["intents"]:
-        for pattern in intent["patterns"]:
-            word = nltk.word_tokenize(pattern)
-            words.extend(word)
-            docs_x.append(word)
-            docs_y.append(intent["tag"])
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        word = nltk.word_tokenize(pattern)
+        words.extend(word)
+        docs_x.append(word)
+        docs_y.append(intent["tag"])
 
-        if intent["tag"] not in labels:
-            labels.append(intent["tag"])
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
 
-    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-    words = sorted(list(set(words)))
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+words = sorted(list(set(words)))
 
-    labels = sorted(labels)
+labels = sorted(labels)
 
-    train = []
-    output = []
+train = []
+output = []
 
-    out_empty = [0 for _ in range(len(labels))]
+out_empty = [0 for _ in range(len(labels))]
 
-    for x, doc in enumerate(docs_x):
-        bag = []
+for x, doc in enumerate(docs_x):
+    bag = []
 
-        word = [stemmer.stem(w.lower()) for w in doc]
+    word = [stemmer.stem(w.lower()) for w in doc]
 
-        for w in words:
-            if w in word:
-                bag.append(1)
-            else:
-                bag.append(0)
+    for w in words:
+        if w in word:
+            bag.append(1)
+        else:
+           bag.append(0)
 
-        output_row = out_empty[:]
-        output_row[labels.index(docs_y[x])] = 1
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
 
-        train.append(bag)
-        output.append(output_row)
+    train.append(bag)
+    output.append(output_row)
 
 
-    train = numpy.array(train)
-    output = numpy.array(output)
+train = numpy.array(train)
+output = numpy.array(output)
 
-    with open("data.pickle", "wb") as f:
-        pickle.dump((words, labels, train, output), f)
+    #with open("data.pickle", "wb") as f:
+    #    pickle.dump((words, labels, train, output), f)
 
 #model building
 tf.compat.v1.reset_default_graph()
